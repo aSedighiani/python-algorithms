29012845dfe33f8cb306d0103e1919df1f61ea94
aSedighiani <artin.sedighiani1389@gmail.com>
2023-02-14 17:09:38 +0000

    Count Odd Numbers uploaded

---
+++ CountOddNumbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if low % 2 == 1:
+            low -=1
+        
+        if high % 2 == 1:
+            high += 1
+
+        return int((high - low) / 2)
